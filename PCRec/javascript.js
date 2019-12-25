85710e96fe3192c937486dfd668bca9fd909d9fe
Cynestall <59012684+Cynestall@users.noreply.github.com>
2019-12-26 01:42:49 +0200

    Add files via upload

--- PCRec/javascript.js
+++ PCRec/javascript.js
@@ -11,3 +11,23 @@ var check = document.getElementById("dark").checked;
 	document.getElementById("centerdiv").className = "center";
 }
 }
+
+var menuClicked = 0;
+
+function menuClick() {
+	console.log("clicked1");
+	if (menuClicked == 0) {
+	menuClicked++;
+	console.log("clicked");
+	document.getElementById("menu").style.display = "block";
+	document.getElementById("menu").style.right = "-300px";
+	document.getElementById("content").style.right = "300px";
+	document.body.style.overflow = "hidden";
+} else if (menuClicked == 1) {
+
+	menuClicked = 0;
+	document.getElementById("menu").style.display = "none";
+	document.getElementById("content").style.right = "0px";
+	document.body.style.overflow = "visible";
+}
+}
